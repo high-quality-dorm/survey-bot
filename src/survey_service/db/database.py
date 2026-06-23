10e09398745ca4bce346d14d487a9749a990a328
zpenr <zpenr@bk.ru>
2026-06-23 23:19:11 +0800

    feat: add functions for create and drop tables

--- src/survey_service/db/database.py
+++ src/survey_service/db/database.py
@@ -1,19 +1,27 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 from survey_service.core.config import settings
-
+from typing import AsyncGenerator
 DATABASE_URL = settings.get_db_url()
 
 engine = create_async_engine(url=DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-async def get_session():
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         try:
             yield session
         finally: 
             await session.close()
+
+async def create_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def drop_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
