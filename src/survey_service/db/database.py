1e84e92439adb6399aa1e2c1bc9eab0dba23883d
zpenr <zpenr@bk.ru>
2026-06-23 23:12:28 +0800

    feat: add async def get_session

--- src/survey_service/db/database.py
+++ src/survey_service/db/database.py
@@ -8,6 +8,13 @@ engine = create_async_engine(url=DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+async def get_session():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally: 
+            await session.close()
+
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
     __allow_unmapped__ = True
