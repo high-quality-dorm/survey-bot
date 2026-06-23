4a2b6dc9eeeba08750f562ee403d145a8ebc6543
sanbob <saintsanbob@gmail.com>
2026-06-23 16:21:16 +0800

    chore: add logging

--- src/bot/create_bot.py
+++ src/bot/create_bot.py
@@ -1,9 +1,16 @@
+import logging
 from asyncio import CancelledError
 
 from aiogram import Bot, Dispatcher
 
 from bot.core import settings
 from bot.handlers import main_router
+
+logging.basicConfig(
+    format="%(levelname)s [%(asctime)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
 
 bot = Bot(token=settings.token)
 dp = Dispatcher(drop_pending_updates=True)
@@ -13,10 +20,12 @@ dp.include_router(main_router)
 
 async def run() -> None:
     try:
-        print("bot started")
+        logging.info("Bot is starting")
         await dp.start_polling(bot)
     except (KeyboardInterrupt, CancelledError):
-        ...
+        logging.warning("Bot received stop signal")
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
     finally:
         await bot.session.close()
-        print("bot stopped")
+        logging.info("Bot session closed. Bot stopped")
