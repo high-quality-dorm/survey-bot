4adef4d117ab994b6e6c48c78cfe154a06dbb987
zpenr <zpenr@bk.ru>
2026-06-24 13:13:54 +0800

    feat: implement JSONParser class

---
+++ src/survey_service/main.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+from .schemas import schemas
+from sqlalchemy.ext.asyncio import AsyncSession
+from .db.queries import create_survey
+from .db.database import get_session
+
+class JSONParser():
+    def __init__(self, file: Path):
+        self.filepath = file
+
+    def parse(self) -> schemas.SurveyJSON:
+        with open(self.filepath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return schemas.SurveyJSON(**data)
+    
+    async def create_survey_in_db(self, session: AsyncSession) -> None:
+        survey = self.parse()
+        await create_survey(session, survey)
+
+if __name__ == "__main__":
+    import asyncio
+    
+    async def main():
+        path = Path(__file__).parents[2]/ "test.json"
+        
+        
+        parser = JSONParser(path)
+        
+        async for session in get_session():
+            await parser.create_survey_in_db(session)
+            print(f"Опрос успешно создан из: {path.name}")
+            break  
+    
+    asyncio.run(main())
