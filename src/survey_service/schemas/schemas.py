80a3401db3956c0ca83cbdaf17190f2bd8ce0d93
zpenr <zpenr@bk.ru>
2026-06-24 13:13:13 +0800

    feat: add base pydantic schemas

---
+++ src/survey_service/schemas/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+class Survey(BaseModel):
+    uuid: str
+    title: str
+    description: str|None
+
+    model_config = {'from_attributes': True}
+
+class Answer(BaseModel):
+    answer: str|None
+
+class Question(BaseModel):
+    question: str
+    survey: Survey
+
+class AnswerJSON(Answer):
+    next_question: str|None
+
+class QuestionJSON(BaseModel):
+    question: str
+    name: str
+    answers: list[AnswerJSON]
+
+class SurveyJSON(BaseModel):
+    title: str
+    description: str|None
+    questions: list[QuestionJSON]
