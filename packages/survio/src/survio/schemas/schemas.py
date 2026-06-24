eaa76aefe9af72f8646e022a15d0aab2deab3307
zpenr <zpenr@bk.ru>
2026-06-24 23:37:22 +0800

    feat: add User, Pass, UserPass schemas

--- packages/survio/src/survio/schemas/schemas.py
+++ packages/survio/src/survio/schemas/schemas.py
@@ -22,3 +22,26 @@ class Question(BaseModel):
     answers: list[Answer]
 
     model_config = {"from_attributes": True}
+
+class User(BaseModel):
+    id: int
+    tg_id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class Pass(BaseModel):
+    id: int
+    user_id: int
+    question_id: int
+    answer: Answer
+
+    model_config = {"from_attributes": True}
+
+
+class UserPass(User):
+    passes: Pass
+    
+    model_config = {"from_attributes": True}
+
