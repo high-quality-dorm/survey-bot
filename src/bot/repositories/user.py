aa841dc94e5d0da8a86aabc7f58efd001bec3bd9
sanbob <saintsanbob@gmail.com>
2026-06-27 15:07:45 +0800

    feat(bot): add User model and UserRepository to manage it

---
+++ src/bot/repositories/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from bot.db import get_session
+from bot.models import User
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session: AsyncSession = session
+
+    async def create_user(self, tg_id: int, core_id: str) -> User:
+        async with get_session() as session:
+            user = User(tg_id=tg_id, core_id=core_id)
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+            return user
+
+    async def get_user(self, tg_id: int) -> User:
+        async with get_session() as session:
+            statement = select(User).filter_by(tg_id=tg_id)
+            result = await session.execute(statement)
+            return result.scalar_one_or_none()
