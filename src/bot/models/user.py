b7099c3236d8ecab2207b38f79231402455dd0f0
sanbob <saintsanbob@gmail.com>
2026-06-27 15:07:45 +0800

    feat(bot): add User model and UserRepository to manage it

---
+++ src/bot/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+
+from bot.db import Base
+
+
+class User(Base):
+    tg_id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, unique=True, nullable=False
+    )
+    core_id: Mapped[str] = mapped_column()
