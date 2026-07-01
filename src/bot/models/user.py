73a2179b5a5617a74f04bb63b881a278b2d617ec
sanbob <saintsanbob@gmail.com>
2026-07-01 14:23:46 +0800

    fix(bot): add __tablename__ to User model

--- src/bot/models/user.py
+++ src/bot/models/user.py
@@ -5,6 +5,7 @@ from bot.db import Base
 
 
 class User(Base):
+    __tablename__ = "bot_users"
     tg_id: Mapped[int] = mapped_column(
         BigInteger, primary_key=True, unique=True, nullable=False
     )
