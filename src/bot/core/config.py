e4b069c76da525ca7ac9635da4818a9575c36e2d
sanbob <saintsanbob@gmail.com>
2026-07-01 15:31:53 +0800

    fix(bot): add db_name to config

--- src/bot/core/config.py
+++ src/bot/core/config.py
@@ -8,13 +8,14 @@ class Settings(BaseSettings):
 
     db_host: str
     db_port: str
+    db_name: str
     db_user: str
     db_password: str
 
     @computed_field  # type: ignore[prop-decorator]
     @property
     def db_url(self) -> str:
-        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/bot_db"
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
 
     model_config = SettingsConfigDict(
         env_file=".env",
