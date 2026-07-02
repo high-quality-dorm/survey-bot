edcaf0224c13d31a2cc4404cffdb0958565933ac
sanbob <saintsanbob@gmail.com>
2026-07-02 10:16:03 +0800

    refactor(survio): enchance create_tables for db, move engine to params

--- packages/survio/src/survio/db/database.py
+++ packages/survio/src/survio/db/database.py
@@ -2,12 +2,12 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import (
     AsyncAttrs,
+    AsyncEngine,
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
 from sqlalchemy.orm import DeclarativeBase
-
 from survio.core.config import settings
 
 DATABASE_URL = settings.get_db_url()
@@ -25,7 +25,7 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
             await session.close()
 
 
-async def create_tables() -> None:
+async def create_tables(engine: AsyncEngine) -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
